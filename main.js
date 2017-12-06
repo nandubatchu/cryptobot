f3491c8f291c68e1acbad0485b0b6505182f855e
Yadunandan Batchu <yadunandanbatchu@Yadunandans-iMac.local>
2017-12-06 15:13:14 +0530

    sample_credentials

--- main.js
+++ main.js
@@ -2,22 +2,9 @@ var bittrex = require('node-bittrex-api');
 var credentials = require('./credentials.json');
 bittrex.options(credentials);
 
-// bittrex.getmarketsummaries( function( data, err ) {
+setInterval( run, 5000);
 
-// 	// console.log(data);
-//   if (err) {
-//     return console.error(err);
-//   }
-//   for( var i in data.result ) {
-//     bittrex.getticker( { market : 'BTC-1ST' }, function( ticker ) {
-//       console.log( ticker );
-//     });
-//   }
-// });
-
-setInterval(ticker,5000);
-
-function ticker() {
+function run() {
 	bittrex.getticker( { market : 'BTC-1ST' }, function( ticker ) {
 		console.log( ticker.result );
 	})
